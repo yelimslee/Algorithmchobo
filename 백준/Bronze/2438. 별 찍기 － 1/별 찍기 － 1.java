1419799b5386f51364c5cd2c56215652a2063a40
yelimslee <141470772+yelimslee@users.noreply.github.com>
2024-03-19 17:41:39 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 328 ms, Memory: 18324 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        for(int i=1; i<N+1; i++) {
+            for(int j=0; j<i; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
