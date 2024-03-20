0348895fb0c689682bee735bbe37e323b6ab5922
yelimslee <141470772+yelimslee@users.noreply.github.com>
2024-03-20 20:04:35 +0900

    [Bronze II] Title: 나머지, Time: 120 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,16 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         HashSet<Integer> h = new HashSet<>();
 
-        for(int i=0; i<10; i++) {
-            h.add(scanner.nextInt() % 42);
+        for (int i = 0; i < 10; i++) {
+            h.add(Integer.parseInt(br.readLine()) % 42);
         }
 
-        scanner.close();
         System.out.print(h.size());
     }
 }
