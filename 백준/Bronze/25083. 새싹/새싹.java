9908b50bd4caae4ea10da551981e2b0d5ee8806d
yelimslee <141470772+yelimslee@users.noreply.github.com>
2024-03-20 23:28:29 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13844 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String s = "         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |\n";
+
+        System.out.print(s);
+    }
+}
