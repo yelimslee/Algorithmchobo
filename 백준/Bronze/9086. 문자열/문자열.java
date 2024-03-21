e3d0cb93d9da15ef23132f04470cefd6da2465e9
yelimslee <141470772+yelimslee@users.noreply.github.com>
2024-03-21 11:49:42 +0900

    [Bronze V] Title: 문자열, Time: 140 ms, Memory: 15872 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,6 +12,7 @@ public class Main {
             String s = br.readLine();
             System.out.println(s.charAt(0) + "" + s.charAt(s.length() - 1));
         }
+
         br.close();
 
     }
