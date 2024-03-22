f6d3e9c9e822e14356db8cbf1e44a8bf29b94cd7
yelimslee <141470772+yelimslee@users.noreply.github.com>
2024-03-23 01:21:52 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
-        int[] arr = new int[100];
 
         int count = 0;
         for (int i = 0; i < s.length(); i++) {
