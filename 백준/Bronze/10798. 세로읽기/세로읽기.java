8b9fb81cf874a7088a660b35ad47378c8f544f6b
yelimslee <141470772+yelimslee@users.noreply.github.com>
2024-03-24 17:30:51 +0900

    [Bronze I] Title: 세로읽기, Time: 124 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -12,15 +12,18 @@ public class Main {
             for (int j = 0; j < Math.min(s.length(), 15); j++) {
                 arr[i][j] = s.charAt(j);
             }
+            // 문자가 들어가지 않는 배열은 빈칸으로 저장
             for (int j = s.length(); j < 15; j++) {
                 arr[i][j] = ' ';
             }
         }
 
+        // 긴 문자열을 더할 때 StringBuilder을 사용하는 것이 좋음
+        // 새로운 객체를 생성하는 것이 아니기 때문에 속도도 빠르며 상대적으로 부화가 적음
         StringBuilder sb = new StringBuilder();
         for (int j = 0; j < 15; j++) {
             for (int i = 0; i < 5; i++) {
-                if (arr[i][j] == ' ')
+                if (arr[i][j] == ' ')  // 배열의 값이 빈칸이면 건너뜀
                     continue;
                 sb.append(arr[i][j]);
             }
